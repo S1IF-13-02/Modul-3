aa1bd02baf74ac73079282c815aa74574a298c4f
Ssandi <faridherdiyanto007@gmail.com>
2025-10-03 20:08:15 +0700

    Create kabisat.go

---
+++ Farid Herdiayanto Vitasandi/kabisat/kabisat.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var year int
+
+	fmt.Print("Masukkan tahun: ")
+	fmt.Scan(&year)
+
+	if (year%4 == 0 && year%100 != 0) || (year%400 == 0){
+		fmt.Println(year, "adalah tahun kabisat atau True")
+	}else{
+		fmt.Println(year, "bukan tahun kabisat atau False")
+	}
+}
