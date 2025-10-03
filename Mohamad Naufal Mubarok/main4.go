9213266262d6296e6d49621fe7d79bcd43279564
MohamadNaufal77 <muhammadnaufal56845@gmail.com>
2025-10-04 00:02:30 +0700

    Add files via upload

---
+++ Mohamad Naufal Mubarok/main4.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var celsius float64
+	fmt.Print("Temperatur Celsius: ")
+	fmt.Scan(&celsius)
+
+	// Konversi
+	reamur := celsius * 4 / 5
+	fahrenheit := (celsius * 9 / 5) + 32
+	kelvin := celsius + 273
+
+	// Tampilkan hasil
+	fmt.Printf("Derajat Reamur: %.0f\n", reamur)
+	fmt.Printf("Derajat Fahrenheit: %.0f\n", fahrenheit)
+	fmt.Printf("Derajat Kelvin: %.0f\n", kelvin)
+}
