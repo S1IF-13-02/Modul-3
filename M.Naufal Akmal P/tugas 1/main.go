764fc4709587ded2284bb6991468636337a6bff6
naufalakmal <akmalprabowo27@gmail.com>
2025-10-04 18:32:39 +0700

    Add files via upload

---
+++ M.Naufal Akmal P/tugas 1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var fx float64
+	fmt.Print("Masukkan nilai f(x): ")
+	fmt.Scan(&fx)
+
+	nilaiX :=(2/(fx-5)) - 5
+	fmt.Printf("Nilai X nya adalah %.0f", nilaiX)
+}
