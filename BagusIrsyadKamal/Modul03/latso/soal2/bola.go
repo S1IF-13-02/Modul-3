b356bd2239ce7f3662c31dfa0d37b68406a62934
Bagus <bagusirsyad1602@gmail.com>
2025-10-06 00:07:36 +0700

    Add files via upload

---
+++ BagusIrsyadKamal/Modul03/latso/soal2/bola.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var r float64
+	const pi = 3.14159265
+	fmt.Print("Jejari = ")
+	fmt.Scan(&r)
+	volume := (4.0 / 3.0) * pi * r * r * r
+	luas := 4 * pi * r * r
+	fmt.Printf("Bola dengan jejari %.0f memiliki volume %.4f dan luas kulit %.4f\n", r, volume, luas)
+}
