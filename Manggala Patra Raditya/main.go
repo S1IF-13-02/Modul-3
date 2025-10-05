b8c64311faf8456b9de51458331a9b9411f30488
manggala-patra <manggala.kluk13@gmail.com>
2025-10-05 22:50:16 +0700

    Add files via upload

--- Manggala Patra Raditya/main.go
+++ Manggala Patra Raditya/main.go
@@ -1,11 +1,13 @@
 package main
-
 import "fmt"
 
 func main() {
-	var x float64
-	fmt.Print("Masukkan nilai x: ")
-	fmt.Scanln(&x)
-	fx := 2/(x+5) + 5
-	fmt.Printf("Nilai f(x) adalah: %g\n", fx)
+var r int
+const PI float64 = 3.1415926535
+
+fmt.Print("Masukkan jari-jari bola: ")
+fmt.Scan(&r)
+volume := (4.0 / 3.0) * PI * float64(r) * float64(r) *float64(r)
+luas := 4 * PI * float64(r) * float64(r)
+fmt.Printf ("Bola dengan jari-jari %d memiliki volume %.4f dan luas kulit %.4f\n", r, volume, luas)
 }
