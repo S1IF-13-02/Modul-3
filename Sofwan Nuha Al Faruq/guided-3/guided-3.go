6d15d465e46c102aa7c96d5d3a33a1c172a42e5a
Sofwan Nuha Al Faruq <111630939+nuha24k@users.noreply.github.com>
2025-10-05 20:40:17 +0700

    Add Files Via Upload

---
+++ Sofwan Nuha Al Faruq/guided-3/guided-3.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	const Dollar = 15000
+	var nilaiRupiah int
+	fmt.Print("Masukkan nilai rupiah: ")
+	fmt.Scan(&nilaiRupiah)
+
+	convert := nilaiRupiah / Dollar
+	fmt.Printf("Hasilnya adalah %d", convert)
+}
